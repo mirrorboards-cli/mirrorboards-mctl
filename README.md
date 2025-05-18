4539c32d7d68aac91234070485935c077bb86459
lunacrafts <lunacrafts@protonmail.com>
2025-05-18 10:17:57 +0200

    clean

--- README.md
+++
@@ -1,3 +0,0 @@
-```bash
-cargo install --git ssh://git@github.com/mirrorboards/mirrorboards-mctl.git --config net.git-fetch-with-cli=true
-```
--- src/commands/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod sync;
-pub mod status;
-
-// Re-export specific functions for easier access
-pub use sync::{sync_repositories, save_repositories};
-pub use status::check_status;
