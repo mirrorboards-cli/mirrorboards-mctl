70fbeb716a91429fc4adcd24cfeb0bf2d85e393c
lunacrafts <lunacrafts@protonmail.com>
2025-05-22 18:33:33 +0200

    Automated commit via save function

--- mirror-sdk/src/models/mod.rs
+++
@@ -1,7 +0,0 @@
-//! Models for the Mirror SDK.
-
-pub mod mirror_config;
-pub mod repository;
-
-pub use mirror_config::MirrorConfig;
-pub use repository::{Repository, RepositoryBuilder};
--- mirror-sdk/src/utils/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Utility functions for the Mirror SDK.
-
-pub mod validation;
-
-pub use validation::{
-    validate_repository, validate_config, check_path_conflicts,
-    check_duplicate_ids, validate_origin, validate_path,
-};
